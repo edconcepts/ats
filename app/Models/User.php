94b4bce037560467e3cd8d4da3bf49174ca689ca
alirezaporthos <alirezaportous@gmail.com>
2023-05-14 20:49:44 +0330

    Add a complete roles to users, middleware, database tables ...

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    // relationships
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory()->create([
+            'name' => 'Test Hr User',
+            'email' => 'hr@example.com',
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test Store Manager User',
+            'email' => 'storemanager@example.com',
+        ]);
+
+    }
+}
