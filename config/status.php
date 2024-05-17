26cb9ab45061f861a50ff2b334112f02e303e7ff
Yinci <yannisch@hotmail.com>
2024-05-17 10:24:13 +0200

    3 and 9 also fixed

--- config/status.php
+++ config/status.php
@@ -2,6 +2,6 @@
 
 return [
     'archive_status_id' => 1,
-    'fixed_status_ids' => [2, 7],
+    'fixed_status_ids' => [2, 3, 7, 9],
     'store_manager_end_interview_email_status_ids' => [3, 9],
 ];
